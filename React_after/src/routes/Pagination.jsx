dd384e4071de6c256353ce364008ca537df01597
QuangViDang <vidangquang1203@gmail.com>
2023-07-17 23:22:59 +0700

    hoanThienLanding

--- React_after/src/routes/Pagination.jsx
+++ React_after/src/routes/Pagination.jsx
@@ -1,19 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import './stylePagination.scss'
 import Table_card from '../features/pageTable/Table_card'
 
 export default function Pagination() {
-    const [inputValue, setInputValue] = useState({ value: '', error: '' })
+    const [number, setNumber] = useState({
+        page: 1,
+        line: 0,
+        start: 0,
+        end: 0,
+        isChooseTable: 1,
+    })
+    const [inputValue, setInputValue] = useState({
+        value: '',
+        error: '',
+        isOk: 'none',
+    })
+
+    const listItem = [
+        { name: 'id', label: 'ID' },
+        { name: 'title', label: 'Title' },
+    ]
+    const [arrayValue, setArrayValue] = useState({ total: [], single: [] })
+
+    const GET_DATA_USER = async () => {
+        try {
+            const reponse = await fetch(
+                'https://jsonplaceholder.typicode.com/albums'
+            )
+            const resuft = await reponse.json()
+            setArrayValue({ ...arrayValue, total: resuft })
+            // console.log(arrayValue.total)
+        } catch (error) {
+            console.log('Error: ', error)
+        }
+    }
+    useEffect(() => {
+        GET_DATA_USER()
+    }, [])
+
     const inputHandle = (e, total) => {
-        if (e.target.value > 0 && e.target.value < total) {
-            console.log(e.target.value)
+        setInputValue({ ...inputValue, isOk: 'none' })
+        console.log(inputValue.isOk)
+        if (
+            !isNaN(e.target.value) &&
+            e.target.value > 0 &&
+            e.target.value < total
+        ) {
             setInputValue({
                 ...inputValue,
                 value: e.target.value,
-            })
-            setInputValue({
-                ...inputValue,
                 error: '',
             })
         } else if (e.target.value === '') {
@@ -29,13 +65,64 @@ export default function Pagination() {
         }
     }
 
+    const handleSave = () => {
+        !inputValue.value
+            ? setInputValue({
+                  ...inputValue,
+                  error: ' Bạn cần nhập ĐÚNG!',
+              })
+            : setInputValue({ ...inputValue, isOk: 'inline' })
+
+        const pageS = arrayValue.total.length / inputValue.value
+        setNumber({
+            ...number,
+            page: Math.ceil(pageS),
+            line: inputValue.value,
+            start: 0,
+            end: number.line,
+        })
+
+        let singleArray = arrayValue.total.slice(number.start, number.end)
+        setArrayValue({ ...arrayValue, single: singleArray })
+        console.log(number)
+    }
+
+    const PaginationSmall = Array.from(
+        { length: number.page - 2 },
+        (_, index_) => {
+            const index = index_ + 2
+            return (
+                <a
+                    onClick={() => handleSetPage(index)}
+                    className={number.isChooseTable === index ? 'active' : ''}
+                    key={index}
+                >
+                    {index}
+                </a>
+            )
+        }
+    )
+    const handleSetPage = (index) => {
+        console.log(number)
+        setNumber({
+            ...number,
+            isChooseTable: index,
+            end: index * number.line,
+            start: number.end - number.line,
+        })
+
+        let singleArray = arrayValue.total.slice(number.start, number.end)
+        setArrayValue({ ...arrayValue, single: singleArray })
+        console.log(number)
+    }
+
     return (
         <div id="pageTable">
             <div className="backToHome">
                 <Link to={`/`}>Back to Root</Link>
             </div>
             <div className="topPage">
-                <h1>Chúng tôi có 10 dòng dữ liệu</h1>
+                <h1>Chúng tôi có 5000 dòng dữ liệu</h1>
                 <h4>We have ten lines of data</h4>
                 <h3>Bạn muốn chia thành bao nhiêu dòng 1 trang?</h3>
                 <h5>How many lines do you want each page to have?</h5>
@@ -49,18 +136,111 @@ export default function Pagination() {
                             id="lineNumber"
                             type="text"
                             placeholder="Nhập tại đây"
-                            onChange={(e) => inputHandle(e, 10)}
+                            onChange={(e) =>
+                                inputHandle(e, arrayValue.total.length + 1)
+                            }
                         />
                         <p style={{ color: 'red' }}>{inputValue.error}</p>
                     </div>
-                    <Link to={`/table/page1`}>
-                        <button>OK</button>
-                    </Link>
+                    <button onClick={handleSave}>OK</button>
                 </div>
             </div>
-            <div>
-                <h1>Say hiiiiiiiii!!!!!!!! Mã số: {inputValue.value}</h1>
-                <Table_card isCheck="true" isCheckAll="true" />
+            <div className="pageTableDiv">
+                <h1>
+                    Say hiiiiiiiii!!!!!!!! Number lines is: {inputValue.value}
+                </h1>
+                
+                <div className="pageTableDiv_pagination">
+                    <a
+                        href="#"
+                        onClick={() => {
+                            let newChoose =
+                                number.isChooseTable === 1
+                                    ? number.page
+                                    : number.isChooseTable - 1
+                            setNumber({
+                                ...number,
+                                isChooseTable: newChoose,
+                            })
+                        }}
+                    >
+                        {'<--'} Previous
+                    </a>
+                    <div class="pagination">
+                        <a
+                            href="#"
+                            onClick={() => {
+                                let newChoose =
+                                    number.isChooseTable === 1
+                                        ? number.page
+                                        : number.isChooseTable - 1
+                                setNumber({
+                                    ...number,
+                                    isChooseTable: newChoose,
+                                })
+                            }}
+                        >
+                            &laquo;
+                        </a>
+                        <a
+                            style={{
+                                display: number.page === 1 ? 'none' : 'inline',
+                            }}
+                            href="#"
+                            onClick={() => handleSetPage(1)}
+                            className={
+                                number.isChooseTable === 1 ? 'active' : ''
+                            }
+                        >
+                            1
+                        </a>
+                        {PaginationSmall}
+                        <a
+                            href="#"
+                            onClick={() => handleSetPage(number.page)}
+                            className={
+                                number.isChooseTable === number.page
+                                    ? 'active'
+                                    : ''
+                            }
+                        >
+                            {number.page}
+                        </a>
+                        <a
+                            href="#"
+                            onClick={() => {
+                                let newChoose =
+                                    number.isChooseTable === number.page
+                                        ? 1
+                                        : number.isChooseTable + 1
+                                setNumber({
+                                    ...number,
+                                    isChooseTable: newChoose,
+                                })
+                            }}
+                        >
+                            &raquo;
+                        </a>
+                    </div>
+                    <a
+                        href="#"
+                        onClick={() => {
+                            let newChoose =
+                                number.isChooseTable === number.page
+                                    ? 1
+                                    : number.isChooseTable + 1
+                            setNumber({
+                                ...number,
+                                isChooseTable: newChoose,
+                            })
+                        }}
+                    >
+                        Next {'-->'}
+                    </a>
+                </div><Table_card
+                    arrayValue={arrayValue.single}
+                    listItem={listItem}
+                />
             </div>
         </div>
     )
