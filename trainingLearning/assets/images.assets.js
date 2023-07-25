d9862bf44870712615bd46896eca51bc0f2efccc
QuangViDang <vidangquang1203@gmail.com>
2023-07-25 11:14:13 +0700

    assets in native

---
+++ trainingLearning/assets/images.assets.js
@@ -0,0 +1,31 @@
+import logo from "./image/logo.svg"
+import img_user from "./image/user.svg"
+import banner from "./image/banner.svg"
+import iconPhone from "./icons/iconPhone.svg"
+import iconFacebook from "./icons/iconFacebook.svg"
+import iconGoogle from "./icons/iconGoogle.svg"
+import iconApple from "./icons/iconApple.svg"
+import iconTwitter from "./icons/iconTwitter.svg"
+import iconInstagram from "./icons/iconInstagram.svg"
+import iconFB from "./icons/iconFB.svg"
+import iconTikTok from "./icons/iconTikTok.svg"
+import iconUploader from "./icons/iconUploader.svg"
+import iconCamera from "./icons/iconCamera.svg"
+
+const IMAGE_APP = {
+    img_user,
+    logo,
+    iconPhone,
+    banner,
+    iconFacebook,
+    iconApple,
+    iconGoogle,
+    iconTwitter,
+    iconInstagram,
+    iconFB,
+    iconTikTok,
+    iconUploader,
+    iconCamera
+}
+
+export default IMAGE_APP
