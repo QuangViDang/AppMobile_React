399f42850cc5ab20355d2c176ca52070ebd2f06d
QuangViDang <vidangquang1203@gmail.com>
2023-08-04 20:08:24 +0700

    fix inviroment

---
+++ App.js
@@ -0,0 +1,81 @@
+import { StatusBar } from 'expo-status-bar'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { Ionicons } from '@expo/vector-icons'
+
+import CategoriesScreen from './screens/CategoriesScreen'
+import MealsOverviewScreen from './screens/MealsOverviewScreen'
+import MealDetailScreen from './screens/MealDetailScreen'
+import FavoritesScreen from './screens/FavoritesScreen'
+
+const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
+function DrawerNavigator() {
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerStyle: { backgroundColor: 'blue' },
+                headerTintColor: 'white',
+                sceneContainerStyle: { backgroundColor: '' },
+            }}
+        >
+            <Drawer.Screen
+                name="Categories"
+                component={CategoriesScreen}
+                options={{
+                    title: 'All Categories',
+                    drawerIcon: ({ color, size }) => (
+                        <Ionicons name="list" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Drawer.Screen
+                name="Favorites"
+                component={FavoritesScreen}
+                options={{
+                    drawerIcon: ({ color, size }) => (
+                        <Ionicons name="star" color={color} size={size} />
+                    ),
+                }}
+            />
+        </Drawer.Navigator>
+    )
+}
+export default function App() {
+    return (
+        <>
+            <StatusBar style="dark" />
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: { backgroundColor: 'white' },
+                        headerTintColor: 'black',
+                        contentStyle: { backgroundColor: 'white' },
+                    }}
+                >
+                    <Stack.Screen
+                        name="Drawer"
+                        component={DrawerNavigator}
+                        options={{ headerShown: false, }}
+                    />
+                    <Stack.Screen
+                        name="MealsOverview"
+                        component={MealsOverviewScreen}
+                        options={({ route }) => {
+                            const catId = route.params.categoryId
+                            return {
+                                title: 'Xin chào với ' + catId,
+                            }
+                        }}
+                    />
+                    <Stack.Screen
+                        name="MealDetail"
+                        component={MealDetailScreen}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    )
+}
---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,43 @@
+import MealItem from '../components/MealItem'
+import { MEALS } from '../data/dummy-data'
+
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+
+const MealsOverviewScreen = ({ route }) => {
+    const cardId = route.params.categoryId
+
+    const displayerMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(cardId) >= 0
+    })
+
+    const renderMealItem = (itemData) => {
+        const item = itemData.item
+        const mealItemProps = {
+            id: item.id,
+            title: item.title,
+            imageUrl: item.imageUrl,
+            affordability: item.affordability,
+            complexity: item.complexity,
+            duration: item.duration,
+        }
+        return <MealItem {...mealItemProps} />
+    }
+    return (
+        <View style={styles.header}>
+            <Text>MealsOverviewScreen - {cardId}</Text>
+            <FlatList
+                style={styles.flatList}
+                data={displayerMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+            />
+        </View>
+    )
+}
+
+export default MealsOverviewScreen
+
+const styles = StyleSheet.create({
+    flatList: { margin: 25 },
+    header: { alignItems: 'center' },
+})
